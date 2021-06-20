5ab630b11a1cefb802cffa2398d68ffc090b8d60
OhmachiSeiya <OhmachiSeiya@gmail.com>
2021-06-20 22:47:29 +0900

    fix validates

--- app/models/review.rb
+++ app/models/review.rb
@@ -5,7 +5,8 @@ class Review < ApplicationRecord
   validates :author, presence: true
   validates :content, presence: true
   validates :category_id, presence: true
-  validates :score, presence: true
+  validates :score, presence: true, numericality: {
+    only_interger: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 100 }
   validates :published, presence: true
   validates :portrait, presence: true
 end
