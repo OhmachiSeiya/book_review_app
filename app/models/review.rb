4ac2cc9d4535af0b4ba68e9980cb20dd685f2a17
OhmachiSeiya <OhmachiSeiya@gmail.com>
2021-06-20 22:34:28 +0900

    Localizing into Japanese and set validates

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,11 @@
 class Review < ApplicationRecord
   has_one_attached :portrait
   belongs_to :category
+  validates :title, presence: true
+  validates :author, presence: true
+  validates :content, presence: true
+  validates :category_id, presence: true
+  validates :score, presence: true
+  validates :published, presence: true
+  validates :portrait, presence: true
 end
--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,14 @@ module BookReviewApp
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
+
+    # デフォルトのlocaleを日本語(:ja)にする
+    config.i18n.default_locale = :ja
+    #  i18nの複数ロケールファイルが読み込まれるようpathを通す
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
     
     config.assets.initialize_on_precompile = false
   end
