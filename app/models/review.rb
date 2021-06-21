e2cc853a63262433238a1822e8a4bcdaa8da5b3d
OhmachiSeiya <OhmachiSeiya@gmail.com>
2021-06-21 19:03:51 +0900

    add valitetion to review

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,9 +1,10 @@
 class Review < ApplicationRecord
   has_one_attached :portrait
   belongs_to :category
-  validates :title, presence: true
-  validates :author, presence: true
-  validates :content, presence: true
+
+  validates :title, presence: true, length: {maximum: 20}
+  validates :author, presence: true, length: {maximum: 20}
+  validates :content, presence: true, length: {maximum: 10000}
   validates :category_id, presence: true
   validates :score, presence: true, numericality: {
     only_interger: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 100 }
