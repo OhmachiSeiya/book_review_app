9da2409f2727ffb990bd0ce3436093a9f6823da6
OhmachiSeiya <OhmachiSeiya@gmail.com>
2021-06-21 14:14:03 +0900

    add category to admin

--- app/admin/categories.rb
+++ app/admin/categories.rb
@@ -1,17 +1,17 @@
-ActiveAdmin.register Review do
-  permit_params :title, :published, :author, :category_id, :score, :content, :portrait
+ActiveAdmin.register Category do
+  permit_params :name
 
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
   # Uncomment all parameters which should be permitted for assignment
   #
-  # permit_params :title, :published, :author, :category_id, :score, :content
+  # permit_params :name
   #
   # or
   #
   # permit_params do
-  #   permitted = [:title, :published, :author, :category_id, :score, :content]
+  #   permitted = [:name]
   #   permitted << :other if params[:action] == 'create' && current_user.admin?
   #   permitted
   # end
