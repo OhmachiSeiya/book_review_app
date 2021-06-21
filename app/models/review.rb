f14270b001be5e61837c7e8759b452ff1b04dc70
OhmachiSeiya <OhmachiSeiya@gmail.com>
2021-06-21 19:09:08 +0900

    Revert "add valitetion to review"
    This reverts commit e2cc853a63262433238a1822e8a4bcdaa8da5b3d.

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,10 +1,9 @@
 class Review < ApplicationRecord
   has_one_attached :portrait
   belongs_to :category
-
-  validates :title, presence: true, length: {maximum: 20}
-  validates :author, presence: true, length: {maximum: 20}
-  validates :content, presence: true, length: {maximum: 10000}
+  validates :title, presence: true
+  validates :author, presence: true
+  validates :content, presence: true
   validates :category_id, presence: true
   validates :score, presence: true, numericality: {
     only_interger: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 100 }
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   resources :reviews
-  root "reviews#top"
+  get "/" => "reviews#top"
   get "show_sample1" => "reviews#show_sample1"
   get "show_sample2" => "reviews#show_sample2"
 end
